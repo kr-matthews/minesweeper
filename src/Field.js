360d6e77725799120561c3590dde3d763226ecd1
Kevin Matthews <kvnrmatthews@gmail.com>
2021-08-12 21:45:19 -0700

    Fix click handlers to do nothing when appropriate. Click revealed cells when right number of adjacent flags. Minor display changes.

--- src/Field.js
+++ src/Field.js
@@ -22,9 +22,10 @@ function resetField(
   setField(skeletonField(rows, columns));
 }
 
-// have stack of hidden, non-mine cells
-// reveal each, and if adjCount is 0, add all neighbours to stack
-function cascadeReveal(stack, setRevealCount, field, setField) {
+// have stack of cells (any state and any hasMine)
+// reveal if hidden;
+// and if it isn't a mine and adjCount is 0, then add all neighbours to stack
+function cascadeReveal(stack, setRevealCount, setGameState, field, setField) {
   while (stack.length > 0) {
     let [r, c] = stack.pop();
     // otherwise already processed it earlier in the stack
@@ -33,20 +34,55 @@ function cascadeReveal(stack, setRevealCount, field, setField) {
       let newField = [...field];
       newField[r][c].state = "show";
       setField(newField);
-      // update revealCount
-      setRevealCount((revealCount) => revealCount + 1);
-      // if it's adjCount is 0, add hidden neighbours to stack
-      if (field[r][c].adjCount === 0) {
-        getNeighbours(r, c, field.length, field[0].length).forEach(
-          ([r0, c0]) => {
-            if (field[r0][c0].state === "hide") {
-              stack.push([r0, c0]);
+      if (field[r][c].hasMine) {
+        // ... and  trigger loss
+        setGameState("lost");
+      } else {
+        // update revealCount
+        setRevealCount((revealCount) => revealCount + 1);
+        // if it's adjCount is 0, add hidden neighbours to stack
+        if (field[r][c].adjCount === 0) {
+          getNeighbours(r, c, field.length, field[0].length).forEach(
+            ([r0, c0]) => {
+              if (field[r0][c0].state === "hide") {
+                stack.push([r0, c0]);
+              }
             }
-          }
-        );
+          );
+        }
       }
     }
   }
+}
+
+// reveal all non-flagged, adj cells
+function revealNonFlagNeighbours(
+  r,
+  c,
+  setRevealCount,
+  setGameState,
+  field,
+  setField
+) {
+  let [m, n] = [field.length, field[0].length];
+  cascadeReveal(
+    getNeighbours(r, c, m, n),
+    setRevealCount,
+    setGameState,
+    field,
+    setField
+  );
+}
+
+// check if adj flag count is equal to adj mine count
+function hasFullFlags(r, c, field) {
+  let [m, n] = [field.length, field[0].length];
+  return (
+    field[r][c].adjCount ===
+    getNeighbours(r, c, m, n).filter(([r0, c0]) => {
+      return field[r0][c0].state === "flag";
+    }).length
+  );
 }
 
 function handleLeftClick(
@@ -59,12 +95,17 @@ function handleLeftClick(
   field,
   setField
 ) {
+  // don't do anything if the game is over
+  if (gameState === "won" || gameState === "lost") {
+    return;
+  }
   let cell = field[r][c];
   // generate the mines if this is the first click
-  if (cell.state === "hide" && gameState === "reset") {
+  if (gameState === "reset" && cell.state === "hide") {
     setGameState("ongoing");
     generateMines(r, c, mineCount, field, setField);
   }
+  // now gameState is ongoing
   // now reveal the cell and perform other actions as necessary
   if (cell.state === "hide" && cell.hasMine) {
     // just lost: reveal current cell...
@@ -75,14 +116,26 @@ function handleLeftClick(
     setGameState("lost");
   } else if (cell.state === "hide") {
     // no mine: reveal cell, and iterate on any neighbours with 0 adj mines
-    cascadeReveal([[r, c]], setRevealCount, field, setField);
+    cascadeReveal([[r, c]], setRevealCount, setGameState, field, setField);
     // there is a useEffect to check when the game is won
+  } else if (cell.state === "show" && hasFullFlags(r, c, field)) {
+    revealNonFlagNeighbours(
+      r,
+      c,
+      setRevealCount,
+      setGameState,
+      field,
+      setField
+    );
   }
 }
 
-function handleRightClick(e, r, c, field, setField) {
-  // TODO:
+function handleRightClick(e, r, c, gameState, field, setField) {
   e.preventDefault();
+  // don't do anything if the game is over
+  if (gameState === "won" || gameState === "lost") {
+    return;
+  }
   let cell = field[r][c];
   if (cell.state === "hide") {
     setField((prev) => {
@@ -133,11 +186,12 @@ function Cell({ args }) {
           )
         }
         onContextMenu={(e) =>
-          handleRightClick(e, rowInd, colInd, field, setField)
+          handleRightClick(e, rowInd, colInd, gameState, field, setField)
         }
       >
         {/* TEMP: should be "show" not "hide" */}
-        {state === "show" ? (hasMine ? "M" : adjCount) : state}
+        {state === "show" && (hasMine ? "M" : adjCount)}
+        {state === "flag" && "!!!!"}
         {/* hasMine ? "M" : adjCount */}
       </button>
     </td>
