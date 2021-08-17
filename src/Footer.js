723f254e6150c2b13b399b0718412c1c5f55a4d9
Kevin Matthews <kvnrmatthews@gmail.com>
2021-08-16 22:04:58 -0700

    Improve game-state message in footer.

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,4 @@
+// TODO: add flag count display (or mineCount - flagCount really)
 // TODO: add links to related: https://pwmarcz.pl/kaboom/ and https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/mines.html
 // TODO: timer, if not too difficulty
 // TODO: high-scores, if local storage is easy
@@ -5,11 +6,24 @@
 
 import Rules from "./Rules.js";
 
+function message(gameState) {
+  switch (gameState) {
+    case "reset":
+      return "The field is reset. Click anywhere to begin.";
+    case "ongoing":
+      return "The game is in progress.";
+    case "won":
+      return "Congratulations, you won!";
+    case "lost":
+      return "Uh oh, you uncovered a mine. You lost.";
+  }
+}
+
 function Footer({ args }) {
   let { gameState } = args;
   return (
     <>
-      <p>The game is {gameState}.</p>
+      <p>{message(gameState)}</p>
       <Rules />
     </>
   );
